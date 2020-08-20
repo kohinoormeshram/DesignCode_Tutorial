b5054d2fa4ddbee828ce30c529b0b8067f533afd
kohinoormeshram <kohinoormeshram@gmail.com>
2020-08-20 12:25:31 +0530

    6. Dismiss and Drag anchors
    Added Drag gesture to the bottom card view and set different anchors for minimum and max offset in y-axis movement

--- DesignCode/ContentView.swift
+++ DesignCode/ContentView.swift
@@ -13,6 +13,8 @@ struct ContentView: View {
     @State var show = false
     @State var viewState = CGSize.zero
     @State var showCard = false
+    @State var bottomState = CGSize.zero
+    @State var showFull = false
     
     var body: some View {
         ZStack {
@@ -23,7 +25,7 @@ struct ContentView: View {
                 .animation(
                     Animation.default
                         .delay(0.1)
-//                        .speed(2)
+    //                        .speed(2)
             )
             
             BackCardView()
@@ -84,7 +86,33 @@ struct ContentView: View {
             
             BottomCardView()
                 .offset(x: 0, y: showCard ? 450 : 1000)
+                .offset(y: self.bottomState.height)
                 .animation(.timingCurve(0.2, 0.8, 0.2, 1, duration: 0.6))
+                .gesture(
+                    DragGesture()
+                        .onChanged({ (value) in
+                            self.bottomState = value.translation
+                            if self.showFull {
+                                self.bottomState.height += -300
+                            }
+                            if self.bottomState.height < -300{
+                                self.bottomState.height = -300
+                            }
+                        })
+                        .onEnded { _ in
+                            if(self.bottomState.height > 50){
+                                self.showCard = false
+                            }
+                            if(self.bottomState.height < -100 && !self.showFull) || (self.bottomState.height <= -250 && self.showFull) {
+                                self.bottomState.height = -300
+                                self.showFull = true
+                            }
+                            else{
+                                self.bottomState = .zero
+                                self.showFull = false
+                            }
+                        }
+                )
             
         }
         
